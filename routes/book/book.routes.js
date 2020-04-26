094af5444ad448c3a633dbb47cfe98b4a6a3bf96
Felipe Rocha <felipemota.rocha@gmail.com>
2020-04-26 08:10:22 -0300

    added 'my books' user route

--- routes/book/book.routes.js
+++ routes/book/book.routes.js
@@ -1,69 +1,70 @@
 const express = require("express");
 const Book = require("../../models/book/book.model");
+const auth = require("../../middlewares/auth/auth.middleware");
 
 const router = new express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-        res.status(200).send(await Book.find({}));
-    } catch ({ message }) {
-        status(400).send(message);
-    }
+router.get("/all", async (req, res) => {
+	try {
+		res.status(200).send(await Book.find({}));
+	} catch ({ message }) {
+		status(400).send(message);
+	}
 });
 
 router.get("/:id", async ({ params: { id } }, res) => {
-    try {
-        const book = await Book.findById(id);
-        if (!book) {
-            throw new Error("Book not found.");
-        }
-        res.status(200).send(book);
-    } catch (err) {
-        res.status(404).send(err.message);
-    }
+	try {
+		const book = await Book.findById(id);
+		if (!book) {
+			throw new Error("Book not found.");
+		}
+		res.status(200).send(book);
+	} catch (err) {
+		res.status(404).send(err.message);
+	}
 });
 
 router.get("/:genre", async ({ params: { genre } }, res) => {
-    try {
-        res.status(200).send(await Book.findByGenre(genre));
-    } catch ({ message }) {
-        res.status(404).send(message);
-    }
+	try {
+		res.status(200).send(await Book.findByGenre(genre));
+	} catch ({ message }) {
+		res.status(404).send(message);
+	}
 });
 
 router.post("/", async ({ body }, res) => {
-    try {
-        const { title, availableForLoan } = body;
+	try {
+		const { title, availableForLoan } = body;
 
-        const registeredBook = await Book.findOne({ title });
+		const registeredBook = await Book.findOne({ title });
 
-        if (registeredBook) {
-            registeredBook.availableForLoan += availableForLoan;
-            await registeredBook.save();
-            res.status(200).send(registeredBook);
-        } else {
-            const book = new Book(body);
-            await book.save();
-            res.status(200).send(book);
-        }
-    } catch ({ message }) {
-        res.status(400).send(message);
-    }
+		if (registeredBook) {
+			registeredBook.availableForLoan += availableForLoan;
+			await registeredBook.save();
+			res.status(200).send(registeredBook);
+		} else {
+			const book = new Book(body);
+			await book.save();
+			res.status(200).send(book);
+		}
+	} catch ({ message }) {
+		res.status(400).send(message);
+	}
 });
 
-router.post('/new-loan', async ({ body: { bookIds } }, res) => {
-    try {
-        for (let bookId of bookIds) {
-            const book = await Book.findById(bookId);
-            book.availableForLoan -= 1;
-            book.onLoan += 1;
-            await book.save();
-        }
+router.post("/new-loan", async ({ body: { bookIds } }, res) => {
+	try {
+		for (let bookId of bookIds) {
+			const book = await Book.findById(bookId);
+			book.availableForLoan -= 1;
+			book.onLoan += 1;
+			await book.save();
+		}
 
-        res.status(200).send();
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-})
+		res.status(200).send();
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
 
 module.exports = router;
--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -1,95 +1,102 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 const auth = require("../../middlewares/auth/auth.middleware");
 const User = require("../../models/user/user.model");
+const Book = require("../../models/book/book.model");
 
 const router = new express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-        res.status(200).send(await User.find({}));
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+	try {
+		res.status(200).send(await User.find({}));
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
 });
 
 router.get("/me", auth, async (req, res) => {
-    try {
-        res.status(200).send(req.user);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+	try {
+		res.status(200).send(req.user);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+router.get("/my-books", auth, async ({ user }, res) => {
+	try {
+		const booksPromises = user.books.map(async ({ bookId }) => await Book.findById(bookId));
+		const books = await Promise.all(booksPromises);
+		res.send(books);
+	} catch (err) {
+		res.send();
+	}
 });
 
 router.post("/", async ({ body: { name, email, password } }, res) => {
-    try {
-        const user = new User({ name, email, password });
-        const token = await user.generateAuthToken();
-        await user.save();
-        res.status(201).send({ user, token });
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+	try {
+		const user = new User({ name, email, password });
+		const token = await user.generateAuthToken();
+		await user.save();
+		res.status(201).send({ user, token });
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
 });
 
 router.post("/sign-in", async ({ body: { email, password } }, res) => {
-    try {
-        const user = await User.findByCredentials(email, password);
-        const token = await user.generateAuthToken();
-        res.status(200).send({ user, token });
-    } catch (err) {
-        res.status(404).send(err);
-    }
+	try {
+		const user = await User.findByCredentials(email, password);
+		const token = await user.generateAuthToken();
+		res.status(200).send({ user, token });
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 router.post("/sign-out", auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter(
-            (token) => token.token !== req.token
-        );
+	try {
+		req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
 
-        await req.user.save();
+		await req.user.save();
 
-        res.status(200).send(req.user);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+		res.status(200).send(req.user);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
 });
 
 // Add Books
-router.post('/add-books', auth, async ({ body: { bookIds }, user }, res) => {
-    try {
-        for (let bookId of bookIds) {
-            user.books = user.books.concat({ bookId });
-            await user.save();
-        }
-        res.status(200).send();
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-})
+router.post("/add-books", auth, async ({ body: { bookIds }, user }, res) => {
+	try {
+		for (let bookId of bookIds) {
+			user.books = user.books.concat({ bookId });
+			await user.save();
+		}
+		res.status(200).send();
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
 
 // Google OAuth
-router.post('/oauth/google', async ({ body: { email, name, googleId } }, res) => {
-    try {
-        const foundUser = await User.findOne({ email });
-        // User doesn't have an account
-        if (!foundUser) {
-            const newUser = new User({ googleId, email, name })
-            const token = await newUser.generateAuthToken();
-            res.status(200).send({ user: newUser, token });
-        }
-        // User already has an account
-        if (!foundUser.googleId) {
-            foundUser.googleId = googleId;
-            await foundUser.save();
-        }
-        const token = await foundUser.generateAuthToken();
-        res.status(200).send({ user: foundUser, token })
-    } catch (err) {
-        res.status(500).send(err);
-    }
-})
+router.post("/oauth/google", async ({ body: { email, name, googleId } }, res) => {
+	try {
+		const foundUser = await User.findOne({ email });
+		// User doesn't have an account
+		if (!foundUser) {
+			const newUser = new User({ googleId, email, name });
+			const token = await newUser.generateAuthToken();
+			res.status(200).send({ user: newUser, token });
+		}
+		// User already has an account
+		if (!foundUser.googleId) {
+			foundUser.googleId = googleId;
+			await foundUser.save();
+		}
+		const token = await foundUser.generateAuthToken();
+		res.status(200).send({ user: foundUser, token });
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
 
 module.exports = router;
