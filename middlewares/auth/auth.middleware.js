dc2448d5cd28613a0bbe63cb0a07b2f9abb0021a
Felipe Rocha <felipemota.rocha@gmail.com>
2020-05-11 06:32:24 -0300

    removed 'add-books' user route, moved its functionality to 'new loan' book route

--- middlewares/auth/auth.middleware.js
+++ middlewares/auth/auth.middleware.js
@@ -3,29 +3,25 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET_KEY = require("../../credentials/jwt/jwt.credential");
 
 const auth = async (req, res, next) => {
-    try {
-        const token = req.header("Authorization").replace("Bearer ", "");
-        const decodedToken = jwt.verify(token, JWT_SECRET_KEY);
-        const user = await User.findOne({
-            _id: decodedToken,
-            "tokens.token": token,
-        });
+	try {
+		const token = req.header("Authorization").replace("Bearer ", "");
+		const decodedToken = jwt.verify(token, JWT_SECRET_KEY);
+		const user = await User.findOne({
+			_id: decodedToken,
+			"tokens.token": token,
+		});
 
-        if (!user) {
-            throw new Error(
-                "Falha na autenticação. Faça login e tente novamente."
-            );
-        }
+		if (!user) {
+			throw new Error("Authentication failed. Sign in and try again.");
+		}
 
-        req.user = user;
-        req.token = token;
+		req.user = user;
+		req.token = token;
 
-        next();
-    } catch (err) {
-        res.status(401).send(
-            "Falha na autenticação. Faça login e tente novamente."
-        );
-    }
+		next();
+	} catch (err) {
+		res.status(401).send("Authentication failed. Sign in and try again.");
+	}
 };
 
 module.exports = auth;
--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -23,7 +23,9 @@ router.get("/me", auth, async (req, res) => {
 
 router.get("/my-books", auth, async ({ user }, res) => {
 	try {
-		const booksPromises = user.books.map(async ({ bookId }) => await Book.findById(bookId));
+		const booksPromises = user.books.map(
+			async ({ bookId }) => await Book.findById(bookId)
+		);
 		const books = await Promise.all(booksPromises);
 		res.send(books);
 	} catch (err) {
@@ -54,7 +56,9 @@ router.post("/sign-in", async ({ body: { email, password } }, res) => {
 
 router.post("/sign-out", auth, async (req, res) => {
 	try {
-		req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+		req.user.tokens = req.user.tokens.filter(
+			(token) => token.token !== req.token
+		);
 
 		await req.user.save();
 
@@ -64,39 +68,29 @@ router.post("/sign-out", auth, async (req, res) => {
 	}
 });
 
-// Add Books
-router.post("/add-books", auth, async ({ body: { books }, user }, res) => {
-	try {
-		books.forEach(({ _id, title, author, genre, image }) => {
-			user.books = user.books.concat({ _id, title, author, genre, image });
-		});
-		await user.save();
-		res.status(200).send(user.books);
-	} catch (err) {
-		res.status(400).send(err.message);
-	}
-});
-
 // Google OAuth
-router.post("/oauth/google", async ({ body: { email, name, googleId } }, res) => {
-	try {
-		const foundUser = await User.findOne({ email });
-		// User doesn't have an account
-		if (!foundUser) {
-			const newUser = new User({ googleId, email, name });
-			const token = await newUser.generateAuthToken();
-			res.status(200).send({ user: newUser, token });
+router.post(
+	"/oauth/google",
+	async ({ body: { email, name, googleId } }, res) => {
+		try {
+			const foundUser = await User.findOne({ email });
+			// User doesn't have an account
+			if (!foundUser) {
+				const newUser = new User({ googleId, email, name });
+				const token = await newUser.generateAuthToken();
+				res.status(200).send({ user: newUser, token });
+			}
+			// User already has an account
+			if (!foundUser.googleId) {
+				foundUser.googleId = googleId;
+				await foundUser.save();
+			}
+			const token = await foundUser.generateAuthToken();
+			res.status(200).send({ user: foundUser, token });
+		} catch (err) {
+			res.status(500).send(err);
 		}
-		// User already has an account
-		if (!foundUser.googleId) {
-			foundUser.googleId = googleId;
-			await foundUser.save();
-		}
-		const token = await foundUser.generateAuthToken();
-		res.status(200).send({ user: foundUser, token });
-	} catch (err) {
-		res.status(500).send(err);
 	}
-});
+);
 
 module.exports = router;
